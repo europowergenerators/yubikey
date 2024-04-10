1677fc79c059501c5112c7bc5015b95f182e6874
Bert Proesmans <bert.proesmans@e-powerinternational.com>
2024-04-10 17:14:31 +0200

    Updated script to initialize PIV
    PIV can be used for out-of-band secret sharing and disaster recovery

--- Enable-Yubikey.ps1
+++ Enable-Yubikey.ps1
@@ -1,3 +1,6 @@
+#Requires -Version 7.0
+#Requires -RunAsAdministrator
+
 $INFO = @{'ForegroundColor' = 'White' }
 $USER = @{'ForegroundColor' = 'Yellow' }
 $DONE = @{'ForegroundColor' = 'Green' }
@@ -40,26 +43,6 @@ function Step {
     }
 }
 
-Function Start-RunAsAdministrator() {
-    $CurrentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
-    if ($CurrentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)) {
-        Write-host "Script is running with Administrator privileges!"
-    }
-    else {
-        #Create a new Elevated process to Start PowerShell
-        $ElevatedProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell";
-        $ElevatedProcess.Arguments = "& '" + $script:MyInvocation.MyCommand.Path + "'" 
-        $ElevatedProcess.Verb = "runas"
-        [System.Diagnostics.Process]::Start($ElevatedProcess) | Out-Null
- 
-        # Exit from the current, unelevated, process
-        Write-Host @global:USER 'Launched administrator powershell process.'
-        Exit 
-    }
-}
-
-Start-RunAsAdministrator
-
 $YubiKeyManagerLocation = 'C:\Program Files\Yubico\YubiKey Manager\ykman.exe'
 if (!(Test-Path -Path $YubiKeyManagerLocation -PathType Leaf)) {
     Write-Host @USER 'The Yubikey Manager program is not found at the default installation location! Install the application before continuing'
@@ -94,6 +77,8 @@ $ConnectedKeys | `
     Write-Host
 Wait 'Press enter to start the reset process for _all_ connected keys'
 
+$pivToEnable = $Host.UI.PromptForChoice('PIV re-enable', 'You want te re-enable PIV, for ICT use?', @('&Yes', '&No'), 1)
+
 $ConnectedKeys | `
     ForEach-Object { $i = 0; } {        
     $DeviceSerial = $_
@@ -111,6 +96,10 @@ $ConnectedKeys | `
         'HSMAUTH' = '--disable'
         'U2F'     = '--enable'
         'FIDO2'   = '--enable'
+    }
+
+    if($pivToEnable -eq 0) {
+        $ModesCollection["PIV"] = '--enable'
     }
         
     $InterfaceCollection = @('usb', 'nfc')
@@ -154,6 +143,83 @@ $ConnectedKeys | `
     Result ([ordered]@{
             'Key'            = "$DeviceSerial [$i]"
             'FIDO PIN' = (& { If ($?) { 'OK' } Else { 'Failure' } })
+        })
+}
+
+# Personal Identity Verification (PIV)
+#
+# PIV is a hardware and protocol standard for authenticating individual persons to a computersystem.
+# The most basic form of PIV, for the mindset of an administrator, is as follows;
+#   - There exist secured devices that have storage slots (similar to registers) for crypthographic keys
+#   - The protocol defines a fixed set of functions for cryptographically signing and encrypting data
+#   - Security is provided through the use of PIN/PUK/Management secrets
+#     The PIN and PUK are 8 bytes (characters) long, the management secret is a DES-key of 24 bytes
+#
+# Yubikey extension to the PIV standard;
+#   - Management key can also be an AES key of 32 bytes
+#   - Functionality can be secured through secondary action; "Touching the key"
+#     The PIV standard doesn't define any second factor besides entering the PIN code
+#
+# The PIN and Management secrets are bound to the owner of the key.
+# The PUK secret is kept by the administrator.
+#
+# The management key is used to do maintenance on the key, like creating new secrets, importing
+# private keys/certificates.
+if($pivToEnable -eq 0) {
+    $ConnectedKeys | `
+    ForEach-Object { $i = 0; } {        
+    $i++;
+    $DeviceSerial = $_
+    
+    Step -Description 'Initiating PIV reset' ([ordered]@{
+        'Key' = "$DeviceSerial [$i]"
+    })
+    & $YKCli --device $DeviceSerial piv reset
+    Result ([ordered]@{
+            'Key'        = "$DeviceSerial [$i]"
+            'PIV Reset' = (& { If ($?) { 'OK' } Else { 'Failure' } })
+        })
+    }
+
+    $ConnectedKeys | `
+    Foreach-Object { $i = 0; } {
+    $i++;
+    $DeviceSerial = $_
+    
+    $DefaultAccess = [System.Collections.ArrayList]@(
+        '--pin', '123456',
+        '--management-key', '010203040506070801020304050607080102030405060708'
+    )
+
+    # Retry count of 1 means there are no retries
+    & $YKCli --device $DeviceSerial piv access set-retries 3 1 --force @DefaultAccess
+    Result ([ordered]@{
+            'Key'            = "$DeviceSerial [$i]"
+            'PIV PIN RETRIES' = (& { If ($?) { 'OK' } Else { 'Failure' } })
+        })
+
+    # Lock out PUK code by providing a wrong PUK
+    & $YKCli --device $DeviceSerial piv access change-puk --puk 000000 --new-puk 000000
+    Result ([ordered]@{
+            'Key'            = "$DeviceSerial [$i]"
+            'PUK LOCKOUT' = (& { If (-not $?) { 'OK' } Else { 'Failure' } })
+        })
+
+    # Put key into PIN-Protected mode.
+    # This generates a new management key and protects it with the PIN. This way the management key
+    # is uniquely set and automatically unlocked when running management functions.
+    & dotnet run --project '.\PIVSetupPinOnly' -- $DeviceSerial
+    Result ([ordered]@{
+            'Key'            = "$DeviceSerial [$i]"
+            'PIV PIN-PROTECTED-MODE' = (& { If ($?) { 'OK' } Else { 'Failure' } })
+        })
+    }
+
+    # Change PIN from default
+    & $YKCli --device $DeviceSerial piv access change-pin --pin 123456
+    Result ([ordered]@{
+            'Key'            = "$DeviceSerial [$i]"
+            'PIV PIN' = (& { If ($?) { 'OK' } Else { 'Failure' } })
         })
 }
 
---
+++ PIVSetupPinOnly/Program.cs
@@ -0,0 +1,34 @@
+﻿using Yubico.YubiKey;
+using Yubico.YubiKey.Piv;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length == 0) throw new ArgumentOutOfRangeException(nameof(args), "You have to provide a serial number for the security key");
+
+        var targetKey = YubiKeyDevice.FindAll().FirstOrDefault(x => x.SerialNumber?.ToString() == args[0]) ?? throw new InvalidOperationException("There is no key attached with the specified serial number");
+        using var PIVsession = new PivSession(targetKey);
+
+        PIVsession.KeyCollector = (data) =>
+        {
+            if (data is null) return false;
+            if (data.IsRetry) throw new InvalidOperationException("The yubikey PIN is not the default PIN. Reset the device first!");
+
+            switch (data.Request)
+            {
+                case KeyEntryRequest.Release:
+                    return true;
+                case KeyEntryRequest.VerifyPivPin:
+                    var currentPin = "123456"u8.ToArray();
+                    data.SubmitValue(currentPin);
+                    return true;
+                default:
+                    break;
+            }
+
+            return false;
+        };
+        PIVsession.SetPinOnlyMode(PivPinOnlyMode.PinProtected);
+    }
+}
